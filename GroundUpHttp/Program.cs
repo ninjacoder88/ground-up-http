4b268acd2a4c57f90f7c1ce8357bb3d7e4e71bb1
ninjacoder88 <thomasharris88@gmail.com>
2026-06-22 21:31:47 -0500

    added middleware

--- GroundUpHttp/Program.cs
+++ GroundUpHttp/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+WebApplication app = WebApplicationBuilder.Build();
+
+app.UseLogging();
+
+app.Use(async (context, next) =>
+{
+    await next(context);
+});
+
+app.Run();
